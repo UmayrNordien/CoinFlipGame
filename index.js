4251eb466147500819cb239436b293927aa0f179
Umayr Nordien <114750267+UmayrNordien@users.noreply.github.com>
2023-06-29 13:23:22 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+let heads = 0;
+let tails = 0;
+let coin = document.querySelector(".coin");
+let flipBtn = document.querySelector("#flip-button");
+let resetBtn = document.querySelector("#reset-button");
+
+flipBtn.addEventListener("click", () => {
+    let i = Math.floor(Math.random() * 2);
+    coin.style.animation = "none";
+    if (i) {
+        setTimeout(function () {
+            coin.style.animation = "spin-heads 3s forwards";
+        }, 100);
+        heads++;
+    } else {
+        setTimeout(function () {
+            coin.style.animation = "spin-tails 3s forwards";
+        }, 100);
+        tails++;
+    }
+    setTimeout(updateStats, 3000);
+    disableButton();
+});
+
+function updateStats() {
+    document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
+    document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
+}
+
+function disableButton() {
+    flipBtn.disabled = true;
+    setTimeout(function () {
+        flipBtn.disabled = false;
+    }, 3000);
+}
+
+resetBtn.addEventListener("click", () => {
+    coin.style.animation = "none";
+    heads = 0;
+    tails = 0;
+    updateStats();
+})
+
+const resetButton = document.getElementById("reset-button");
+
+resetButton.addEventListener("click", function() {
+  resetButton.classList.add("purple-button");
+  setTimeout(function() {
+    resetButton.classList.remove("purple-button");
+  }, 1000); // change 2000 to the number of milliseconds you want the button to remain purple
+});
